60a0475810621668e96cfbb2450c8e7abee3cfa9
codie0226 <legoholic226@naver.com>
2026-02-12 18:39:04 +0900

    fix: 쿠키 설정 방식 strict에서 lax로 변경

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -41,7 +41,7 @@ export class UserController extends Controller {
     req.res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7일
     });
 
